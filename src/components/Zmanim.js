f5c713d13dc94d6957aa4cccfaef2f6686481862
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 20:42:45 +0000

    Zmanim.js: changed

---
+++ src/components/Zmanim.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+class Myzman extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      parsha: '',
+      candle: '',
+      havdalah: '',
+      loading: '',
+      yomtov: '',
+      Motzei: '',
+    }
+  }
+
+  componentDidMount() {
+    // getzmanim = () => {
+    fetch(
+      'https://www.hebcal.com/shabbat/?cfg=json&geo=zip&zip=11021&m=55&leyning=off'
+    )
+      .then(r => r.json())
+      .then(data => {
+        let zman = data.items
+        if (zman[0].category === 'holiday') {
+          let yomtova = zman[0].hebrew
+          let havdalah = zman[3].title
+          this.setState({
+            candle: zman[1].title,
+            yomtov: yomtova,
+            havdalah: `No melacha maybe done before ${havdalah}`,
+            Motzei: `Marriv  ${zman[5].title}`,
+          })
+        } else {
+          let havdalaha = zman[2].title
+          this.setState({
+            parsha: `This weeks parsha is,  ${zman[1].hebrew}`,
+            candle: zman[0].title,
+            havdalah: `Motzei shabbos Marriav/ ${havdalaha}`,
+          })
+        }
+
+        console.log(this.state)
+      })
+  }
+
+  render() {
+    return (
+      <div className="col-4" style={{textAlign: 'center'}}>
+        {/* {this.state.loading != true ? console.log("error ")
+      :   */}
+        <h2>Great Neck Zmanim</h2>
+        <div>{this.state.parsha ? this.state.parsha : this.state.yomtov}</div>
+        <div>{this.state.candle}</div>
+        <div> </div>
+        <div>{this.state.havdalah}</div>
+        <div>{this.state.Motzei}</div>
+      </div>
+    )
+  }
+}
+
+export default Myzman
