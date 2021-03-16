5cdb3286db29a317a825120fa232f18a052b6286
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 20:45:56 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -12,7 +12,7 @@ export default class SectionFeatures extends React.Component {
         return (
             <section id={_.get(section, 'section_id', null)} className={'block features-block bg-' + _.get(section, 'background', null) + ' outer'}>
                 < Myzman />
-                <div></div>
+                <div><br/></div>
               <div className="block-header inner-small">
                 {_.get(section, 'title', null) && (
                 <h2 className="block-title">{_.get(section, 'title', null)}</h2>
